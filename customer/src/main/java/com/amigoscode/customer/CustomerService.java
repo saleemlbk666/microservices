a832bb9b469e547bbb46d9fab126e0b91410bae1
Saleem Bhasha <saleemlbk666@gmail.com>
2024-08-15 16:04:23 +0530

    added 'open feign client' to avoid duplicate fraud check response class

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,12 +14,14 @@ public class CustomerService{
 
     private final RestTemplate restTemplate;
 
+    private final FraudClient fraudClient;
+
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName())
                 .email(request.email()).build();
         customerRepository.saveAndFlush(customer);
-        FraudCheckResponse fraudCheckResponse =
-                restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudSter(customer.getId());
 
         if(fraudCheckResponse.isFraudSter()){
             throw new IllegalStateException("fraudster");
